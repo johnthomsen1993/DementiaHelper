1880e049a7ea2bc8fe8778e588d10569737a3ee9
Claus <201271020@iha.dk>
2017-02-09 11:30:36 +0100

    Added space

--- DementiaHelper/DementiaHelper/DementiaHelper.Droid/MainActivity.cs
+++ DementiaHelper/DementiaHelper/DementiaHelper.Droid/MainActivity.cs
@@ -15,6 +15,7 @@ namespace DementiaHelper.Droid
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
+
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
