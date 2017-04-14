143a1027c7d09efa645ef0e138c3b6a25e184321
DESKTOP-SS05BON\Tinno <jens.borg.juelsbak@gmail.com>
2017-04-14 17:43:31 +0200

    Added Fody for OnNotifyPropertyChanged -> Use [ImplementPropertyChanged] attribute

--- DementiaHelper/DementiaHelper/DementiaHelper/PageModels/ShoppingListPageModel.cs
+++ DementiaHelper/DementiaHelper/DementiaHelper/PageModels/ShoppingListPageModel.cs
@@ -3,6 +3,7 @@ using DementiaHelper.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -10,15 +11,18 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using DementiaHelper.Services;
 using Newtonsoft.Json.Linq;
+using PropertyChanged;
 using Xamarin.Forms;
 
 namespace DementiaHelper.PageModels
 {
+    [ImplementPropertyChanged]
     public class ShoppingListPageModel : FreshMvvm.FreshBasePageModel
     {
         public const string URI_BASE = "http://dementiahelper.azurewebsites.net/api/values/shoppinglist/";
         public const string URI_BASE_TEST = "http://localhost:29342/api/values/shoppinglist/";
         public ShoppingList ShoppingList { get; set; }
+        public ObservableCollection<ShoppingListDetail> ShoppingListDetails { get; set; }
 
         public ShoppingListPageModel()
         {
@@ -27,6 +31,8 @@ namespace DementiaHelper.PageModels
             {
                 var shoppinglist = await GetShoppingList(8);
                 ShoppingList.ShoppingListDetails = shoppinglist.ShoppingListDetails;
+                ShoppingList.ShoppingListId = shoppinglist.ShoppingListId;
+                ShoppingListDetails = ShoppingList.ShoppingListDetails;
             });
         }
 
@@ -59,19 +65,19 @@ namespace DementiaHelper.PageModels
             var list = dict.Where(x => x.Key.Contains("ShoppingList")).Select(x => x.Value).ToList().FirstOrDefault() as IEnumerable<object>;
             foreach (var obj in list)
             {
-                var JsonContainer = obj as JContainer;
+                var jsonContainer = obj as JContainer;
 
-                var shoppingListDetailId = JsonContainer.SelectToken("ShoppingListDetailId");
-                var bought = JsonContainer.SelectToken("Bought");
-                var quantity = JsonContainer.SelectToken("Quantity");
+                var shoppingListDetailId = jsonContainer.SelectToken("ShoppingListDetailId");
+                var bought = jsonContainer.SelectToken("Bought");
+                var quantity = jsonContainer.SelectToken("Quantity");
 
                 //Product
-                var jsonProduct = JsonContainer.SelectToken("ProductForeignKey");
+                var jsonProduct = jsonContainer.SelectToken("ProductForeignKey");
                 var productName = jsonProduct.SelectToken("ProductName");
                 var productId = jsonProduct.SelectToken("ProductId");
 
                 //ShoppingList
-                var jsonShoppingList = JsonContainer.SelectToken("ShoppingListForeignKey");
+                var jsonShoppingList = jsonContainer.SelectToken("ShoppingListForeignKey");
                 var shoppingListId = jsonShoppingList.SelectToken("ShoppingListId");
 
                 tempShoppingList.ShoppingListId = shoppingListId.ToObject<int>();
